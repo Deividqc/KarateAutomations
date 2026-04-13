7c14fa2e809ba2328fea33ad7929a579742d2860
deividqc <jesusdavidquispecasas@gmail.com>
2026-04-13 17:31:49 -0500

    Mas DB exercises

--- src/test/java/helpers/DbHandler.java
+++ src/test/java/helpers/DbHandler.java
@@ -3,13 +3,16 @@ package helpers;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import net.minidev.json.JSONObject;
 
 public class DbHandler {
 
     private static String connectionUrl = "jdbc:mysql://localhost:3306/myloandb?user=root&password=Gabylu212208.";
       
-        public static void addNewJobWithId(int clientId, String firstName, String lastName, String email, String phoneNumber, String dateJoined) {
+        public static void addNewClientWithId(int clientId, String firstName, String lastName, String email, String phoneNumber, String dateJoined) {
         // Usamos PreparedStatement para evitar inyección SQL y manejar variables fácilmente
         String sql = "INSERT INTO clients (ClientID, FirstName, LastName, Email, PhoneNumber, DateJoined) " + "VALUES (?, ?, ?, ?, ?, ?)";
 
@@ -34,21 +37,31 @@ public class DbHandler {
             throw new RuntimeException("Error en DB: " + e.getMessage());
         }
     }
-}
 
+    public static JSONObject getDataLoanByClientId(int clientId) {
+        JSONObject json = new JSONObject();
 
-
-        /*public static void addNewJobWithName() {
-            try(Connection connection = DriverManager.getConnection(connectionUrl)){
-                connection.createStatement().execute("Insert into myloandb.clients(ClientID, FirstName, LastName, Email, PhoneNumber, DateJoined ) values(53,'Juan','Garcia','juangarcia@hotmail.com','555-9889-3333','2023-02-21')");
-                connection.commit();
-            }  catch(SQLException e){
-                e.printStackTrace();
+            try (Connection connection = DriverManager.getConnection(connectionUrl)) {
+                 ResultSet rs = connection.createStatement().executeQuery("Select c.firstname,c.lastname, cl.loanId, cl.loanAmount\n" + //
+                                          "from currentloans cl\n" + //
+                                          "inner join clients c on c.clientID=cl.clientID\n" + //
+                                          "where cl.ClientID= " + clientId);
+                 rs.next();
+                    json.put("FirstName", rs.getString("c.firstname"));
+                    json.put("LastName", rs.getString("c.lastname"));
+                    json.put("LoanID", rs.getInt("cl.loanId"));
+                    json.put("LoanAmount", rs.getInt("cl.loanAmount"));
+                
+                    //System.out.println("Datos obtenidos para ClientID " + clientId + ": " + json.toJSONString());
+                } 
+             catch (SQLException e) {
+                throw new RuntimeException("Error en DB: " + e.getMessage());
             }
-             
-        
-           
-        }*/
 
+        return json;
+        // Aquí podrías implementar un método para consultar datos de la base usando el clientId
+        // y devolverlos a Karate para validación. Esto es solo un ejemplo de cómo podrías estructurarlo.
+    
+    }
 
-
+}
--- target/test-classes/Conduit/feature/WorkWithDB.feature
+++ target/test-classes/Conduit/feature/WorkWithDB.feature
@@ -3,9 +3,9 @@ Feature: Work with DB
 Background: connect to db
     * def dbConfig = Java.type('helpers.DbHandler')
 
-@debug
+
 Scenario Outline: Seed database with a  new job
-    * eval dbConfig.addNewJobWithId(<IdClient>, "<FirstName>", "<LastName>", "<Email>", "<PhoneNumber>", "<DateJoined>")
+    * eval dbConfig.addNewClientWithId(<IdClient>, "<FirstName>", "<LastName>", "<Email>", "<PhoneNumber>", "<DateJoined>")
 
     Examples:
     | IdClient | FirstName | LastName    | Email                       | PhoneNumber  |DateJoined |
@@ -13,3 +13,19 @@ Scenario Outline: Seed database with a  new job
     | 56       | Jose      | Landauri    | jose.landauri@hotmail.com   | 555-9902-4444|2023-02-23 |
     | 57       | Maria     | Rodriguez   | maria.rodriguez@hotmail.com | 555-9903-4444|2023-02-24 |
     | 58       | Ana       | Perez       | ana.perez@hotmail.com       | 555-9904-4444|2023-02-25 |
+
+@debug
+Scenario Outline: Get loan data from a client by id
+    * def loans = dbConfig.getDataLoanByClientId(<ClientId>)
+    * print loans.FirstName
+    * print loans.LastName
+    * print loans.LoanID
+    * print loans.LoanAmount
+
+    Examples:
+    | ClientId |
+    | 1        |
+    | 2        |
+    | 3        |
+    | 4        | 
+
